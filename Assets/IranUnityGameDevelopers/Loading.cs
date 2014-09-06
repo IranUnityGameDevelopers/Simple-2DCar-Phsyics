19b15a116022dd58ff36734fac2e7283490b1cd4
Amin Sojoudi <masojoudi@gmail.com>
2014-09-06 12:31:09 +0430

    * added unity Project so everyone can clone it , update it and push it back to server easily

---
+++ Assets/IranUnityGameDevelopers/Loading.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Loading : MonoBehaviour {
+
+	public string NextSceneToLoad;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(ShowSplash());
+	}
+
+
+	IEnumerator ShowSplash()
+	{
+		yield return new WaitForSeconds(3);
+		Application.LoadLevel(NextSceneToLoad);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
